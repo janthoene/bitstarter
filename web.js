0524bb388d9c5cf97ee9f268f1fbe4ce7657d587
janthoene <jan.thoene@gmx.de>
2014-01-20 12:40:39 +0000

    Edited web.js to read index.html

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+var HTMLImport = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.writeHead(200, {
+      'Content-type': 'text/html; charset=utf-8'
+ });
+ respond.end(HTMLImport);
+
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
