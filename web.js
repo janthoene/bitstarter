b89014593f6280086342f2c1d5e47da41d7ec927
janthoene <jan.thoene@gmx.de>
2014-02-03 16:14:26 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile="index.html";
 
 var app = express.createServer(express.logger());
-var HTMLImport = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.writeHead(200, {
-      'Content-type': 'text/html; charset=utf-8'
- });
- response.end(HTMLImport);
-
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 8080;
